4b94147494d0509b660448eaf3c0b9ccd491e612
Navjotdyal <jotdyal681>
2023-07-05 17:04:34 +0530

    print hello world in loop

---
+++ greeting.cpp
@@ -0,0 +1,11 @@
+  #include<iostream>
+  using namespace std;
+  int main()
+  {
+    //10 time declare good mormring
+    //automate repition
+    for(int i=1; i<=5;i++){
+    cout<<"good morning"<<endl;
+    cout<<"hello"<<endl;
+      }
+  }

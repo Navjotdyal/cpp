e87af08598a6ffc0acbb39c0d2dcd9366bdec8dd
Navjotdyal <jotdyal681>
2023-07-05 17:07:28 +0530

    print hello world in loop

--- printheloworld.cpp
+++ printheloworld.cpp
@@ -4,7 +4,7 @@ int main(){  //n:helloworld
     int n;
     cout<<"enter a number:";
     cin>>n;
- for(int i=1;i<=n;i++)     // n on thei<=n
+ for(int i=1;i<=6;i++)     // n on thei<=n
  {
     cout<<"hello world"<<endl;
  }
